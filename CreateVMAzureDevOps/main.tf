57e15c90b7b2eaec310921fb65acf9210ef14fe3
Garvit Mehta <35095197+garvitm178@users.noreply.github.com>
2023-06-08 16:28:47 +0530

    Update main.tf

--- CreateVMAzureDevOps/main.tf
+++ CreateVMAzureDevOps/main.tf
@@ -5,6 +5,13 @@ terraform {
       version = "=3.0.0"
     }
   }
+  
+  backend "azurerm" {
+    storage_account_name = "aviopt"
+    container_name       = "avimsd"
+    key                  = "terraform.tfstate"
+    access_key           = "++B7LTNi007PI+iLh/6NVOCVtNElvKryUE7PRJSwObClNOGfUixl+hjlGY2h3djKalKbHtDtnbi/+ASt79tvZA=="
+  }
 }   
 
 resource "azurerm_resource_group" "r1" {
